6621fbf54e14d09c06d83cdc09e4355cf82826ec
Mohamed Farrag <mohamed.farrag0@gmail.com>
2014-11-11 22:31:49 +0200

    new fix
    fix avg.  response time and top dyno to be calculated for each of the 6
    links
    
    change all the way that the statistics is stored and calculated, and use
    datatables instead of hashtables

--- PocketPlayLabTest/Program.cs
+++ PocketPlayLabTest/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace PocketPlayLabTest
 {
@@ -13,53 +10,87 @@ namespace PocketPlayLabTest
     {
 
         #region variables
-
-        /// <summary>
-        /// store unique links with counter of repeats..
-        /// </summary>
-        static Hashtable htLinks = new Hashtable();
-        /// <summary>
-        /// store unique dyno with counter of repeats..
-        /// </summary>
-        static Dictionary<string, int> dcDyno = new Dictionary<string, int>();
-
         
-        static Regex rxCount_pending_messages = new Regex(@"/api/users/\d+/count_pending_messages");
-        static Regex rxGet_messages = new Regex(@"/api/users/\d+/get_messages");
-        static Regex rxGet_friends_progress = new Regex(@"/api/users/\d+/get_friends_progress");
-        static Regex rxGet_friends_score = new Regex(@"/api/users/\d+/get_friends_score");
-        static Regex rxUser = new Regex(@"/api/users/\d+");
+        static readonly Regex RxCountPendingMessages = new Regex(@"/api/users/\d+/count_pending_messages");
+        static readonly Regex RxGetMessages = new Regex(@"/api/users/\d+/get_messages");
+        static readonly Regex RxGetFriendsProgress = new Regex(@"/api/users/\d+/get_friends_progress");
+        static readonly Regex RxGetFriendsScore = new Regex(@"/api/users/\d+/get_friends_score");
+        static readonly Regex RxUser = new Regex(@"/api/users/\d+");
 
-        private static int totalResponseTime = 0; //in milleseconds..
+        /// <summary>
+        /// store unique links with counter of repeats, response time..
+        /// </summary>
+        static DataTable dtLinks = new DataTable();
+
+        /// <summary>
+        /// store links dyno with count of repeats..
+        /// </summary>
+        static DataTable dtLinkDyno = new DataTable();
 
         #endregion
 
         static void Main(string[] args)
         {
-            ReadLogData();
+            InitialDataTables();
+            bool dataReaded = ReadLogData();
 
-            PrintStatistic();
+            if (dataReaded)
+                PrintStatistic();
         }
-        
 
-        static void ReadLogData()
+        private static void InitialDataTables()
         {
-            StreamReader reader = new StreamReader("sample.log");
+            DataColumn cId = new DataColumn("LinkId", typeof(int));
+            cId.AutoIncrement = true;
+            dtLinks.Columns.Add(cId);
+            cId.AutoIncrementSeed = 1;
+            dtLinks.Columns.Add("Link", typeof(string));
+            DataColumn cCount = new DataColumn("Count", typeof(int));
+            cCount.DefaultValue = 0;
+            dtLinks.Columns.Add(cCount);
+            DataColumn cTime = new DataColumn("ResponseTime", typeof(int));
+            cTime.DefaultValue = 0;
+            dtLinks.Columns.Add(cTime);
 
-            while (!reader.EndOfStream)
+            DataColumn cId2 = new DataColumn("Id", typeof(int));
+            cId2.AutoIncrement = true;
+            cId2.AutoIncrementSeed = 1;
+            dtLinkDyno.Columns.Add(cId2);
+            dtLinkDyno.Columns.Add("LinkId", typeof(int));
+            dtLinkDyno.Columns.Add("Dyno", typeof(string));
+
+            DataColumn cDynoCount = new DataColumn("Count", typeof(int));
+            cDynoCount.DefaultValue = 0;
+            dtLinkDyno.Columns.Add(cDynoCount);
+        }
+
+
+        static bool ReadLogData()
+        {
+            try
             {
-                var readLine = reader.ReadLine();
-                if (readLine != null)
+                StreamReader reader = new StreamReader("sample.log");
+
+                while (!reader.EndOfStream)
                 {
-                    string[] parsedLine = readLine.Split(' ');
+                    var readLine = reader.ReadLine();
+                    if (readLine != null)
+                    {
+                        string[] parsedLine = readLine.Split(' ');
 
-                    CheckLinks(parsedLine);
+                        CheckLinks(parsedLine);
 
-                    CheckDyno(parsedLine);
-
-                    GetResponseTime(parsedLine);
+                        // CheckDyno(parsedLine);
+                    }
                 }
             }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Log file not found");
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -70,39 +101,70 @@ namespace PocketPlayLabTest
         {
             string path = SplitOnEqualSign(parsedLine[4]); // path index is 4
             string key = "";
-            if (rxCount_pending_messages.IsMatch(path))
+
+            #region match link with regex
+
+            if (RxCountPendingMessages.IsMatch(path))
             {
                 key = "GET /api/users/{user_id}/count_pending_messages";
             }
-            else if (rxGet_messages.IsMatch(path))
+            else if (RxGetMessages.IsMatch(path))
             {
                 key = "GET /api/users/{user_id}/get_messages";
             }
-            else if (rxGet_friends_progress.IsMatch(path))
+            else if (RxGetFriendsProgress.IsMatch(path))
             {
                 key = "GET /api/users/{user_id}/get_friends_progresss";
             }
-            else if (rxGet_friends_score.IsMatch(path))
+            else if (RxGetFriendsScore.IsMatch(path))
             {
                 key = "GET /api/users/{user_id}/get_friends_score";
             }
-            else if (rxUser.IsMatch(path))
+            else if (RxUser.IsMatch(path))
             {
                 // this case it may be GET or POST.. check the method
                 string method = SplitOnEqualSign(parsedLine[3]); // method index is 3
                 if (method == "POST")
+                {
                     key = "POST /api/users/{user_id}";
+                }
                 else
+                {
                     key = "GET /api/users/{user_id}";
+                }
             }
+
+            #endregion
 
             // if the key matched any of the regex, count it
             if (!string.IsNullOrEmpty(key))
             {
-                if (htLinks.ContainsKey(key))
-                    htLinks[key] = Convert.ToInt32(htLinks[key]) + 1;
+                int linkId;
+                int time = GetResponseTime(parsedLine);
+               
+                //update the requests count, or add new link
+                var row = dtLinks.AsEnumerable().Where(x => x.Field<string>("Link") == key).FirstOrDefault();
+                if (row != null)
+                {
+                    linkId = Convert.ToInt16(row["LinkId"]);
+                    row.BeginEdit();
+                    row["Count"] = Convert.ToInt32(row["Count"]) + 1;
+                    row["ResponseTime"] = Convert.ToInt32(row["ResponseTime"]) + time;
+                    row.EndEdit();
+                }
                 else
-                    htLinks.Add(key, 1);
+                {
+                    DataRow newLink = dtLinks.NewRow();
+                    newLink["Link"] = key;
+                    newLink["Count"] = 1;
+                    newLink["ResponseTime"] = time;
+                    dtLinks.Rows.Add(newLink);
+
+                    linkId = Convert.ToInt16(newLink["LinkId"]);
+                }
+
+                //store dyno for the current link..
+                GetLinkDyno(parsedLine: parsedLine, linkId: linkId);
             }
         }
 
@@ -110,7 +172,8 @@ namespace PocketPlayLabTest
         /// check and calculate response time
         /// </summary>
         /// <param name="parsedLine">one line of the log file</param>
-        static void GetResponseTime(string[] parsedLine)
+        /// <returns>total response time (connect time  + service Time)</returns>
+        static int GetResponseTime(string[] parsedLine)
         {
             int connect = 0, serviceTime = 0;
             string connectText = SplitOnEqualSign(parsedLine[8]), serviceTimeText = SplitOnEqualSign(parsedLine[9]); // connect index is 8, service time index is 9
@@ -119,23 +182,40 @@ namespace PocketPlayLabTest
             {
                 if (int.TryParse(serviceTimeText.Remove(serviceTimeText.Length - 2, 2), out serviceTime))
                 {
-                    totalResponseTime += connect + serviceTime;
+                    return (connect + serviceTime);
                 }
             }
-
+            return 0;
         }
 
         /// <summary>
-        /// check dyno repeated count
+        /// check dyno for spicific link and count it
         /// </summary>
         /// <param name="parsedLine">one line of the log file</param>
-        static void CheckDyno(string[] parsedLine)
+        /// <param name="linkId">the link Id that will check for</param>
+        static void GetLinkDyno(string[] parsedLine, int linkId)
         {
             string dyno = SplitOnEqualSign(parsedLine[7]); // dyno index is 7
-            if (dcDyno.ContainsKey(dyno))
-                dcDyno[dyno] = Convert.ToInt32(dcDyno[dyno]) + 1;
-            else
-                dcDyno.Add(dyno, 1);
+
+            if (!string.IsNullOrEmpty(dyno))
+            {
+                //check if that dyno is already there for the linkId or not
+                var row = dtLinkDyno.AsEnumerable().Where(x => x.Field<int>("LinkId") == linkId && x.Field<string>("Dyno") == dyno).FirstOrDefault();
+                if (row != null)
+                {
+                    row.BeginEdit();
+                    row["Count"] = Convert.ToInt32(row["Count"]) + 1;
+                    row.EndEdit();
+                }
+                else
+                {
+                    DataRow newLink = dtLinkDyno.NewRow();
+                    newLink["Dyno"] = dyno;
+                    newLink["Count"] = 1;
+                    newLink["LinkId"] = linkId;
+                    dtLinkDyno.Rows.Add(newLink);
+                }
+            }
         }
 
         /// <summary>
@@ -153,24 +233,24 @@ namespace PocketPlayLabTest
 
         private static void PrintStatistic()
         {
-            foreach (var link in htLinks.Keys)
+            foreach (DataRow link in dtLinks.Rows)
             {
-                Console.WriteLine(string.Format("Count: {0}  Link: {1}", htLinks[link], link));
+                // get top dyno
+                var topDyno = dtLinkDyno.AsEnumerable()
+                                        .Where(x => x.Field<int>("LinkId") == Convert.ToInt32(link["LinkId"]))
+                                        .OrderByDescending(o => o.Field<int>("Count")).FirstOrDefault();
+
+                decimal avgTime = Math.Round((Convert.ToInt32(link["ResponseTime"]) / Convert.ToDecimal(link["Count"])),2);
+                string dyno = "-";
+                if (topDyno != null)
+                    dyno = string.Format("{0} ({1} times)", topDyno["Dyno"], topDyno["Count"]);
+
+
+                Console.WriteLine(string.Format("Link: {1}       Requests Count: {0}     Avg. Response Time: {2} ms     Most Dyno: {3}",
+                    link["Count"], link["Link"], avgTime, dyno));
             }
-
-            Console.WriteLine();
-            Console.WriteLine("=============================================");
-            Console.WriteLine("=============================================");
-            Console.WriteLine();
-            Console.WriteLine(string.Format("the average of the response time= {0} ms", (totalResponseTime / htLinks.Count)));
-            Console.WriteLine();
-
-            Console.WriteLine("=============================================");
-            Console.WriteLine("=============================================");
-
-            var x = dcDyno.OrderByDescending(kp => kp.Value).First();
-            Console.WriteLine();
-            Console.WriteLine(string.Format("The dyno that responded the most: {0} ({1} time(s))", x.Key, x.Value));
         }
+
+
     }
 }
